42289d04e4985665c689bf9ae22c315b57e42cd8
Joel Kazadi <kazadijoel36@gmail.com>
2026-09-18 08:41:26 +0200

    Use modern Android back dispatcher and WebView lifecycle

--- android/app/src/main/java/com/vraxis/kickoffstar/MainActivity.java
+++ android/app/src/main/java/com/vraxis/kickoffstar/MainActivity.java
@@ -1,14 +1,38 @@
 package com.vraxis.kickoffstar;
 
+import android.os.Bundle;
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+  private OnBackPressedCallback backCallback;
+
   @Override
-  public void onBackPressed() {
-    if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
-      bridge.getWebView().goBack();
-    } else {
-      super.onBackPressed();
-    }
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    backCallback = new OnBackPressedCallback(true) {
+      @Override public void handleOnBackPressed() {
+        if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+          bridge.getWebView().goBack();
+        } else {
+          setEnabled(false);
+          getOnBackPressedDispatcher().onBackPressed();
+          setEnabled(true);
+        }
+      }
+    };
+    getOnBackPressedDispatcher().addCallback(this, backCallback);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (bridge != null && bridge.getWebView() != null) bridge.getWebView().onPause();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    if (bridge != null && bridge.getWebView() != null) bridge.getWebView().onResume();
   }
 }
